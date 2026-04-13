533edc8370314ec67d69702f14016a8de40e74fc
roseewood <193557890+roseewood@users.noreply.github.com>
2026-04-13 07:06:54 +0530

    updated main.py

--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,17 @@ from database import Base, engine, SessionLocal
 from models import User
 from auth import hash_password, verify_password, create_token
 
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
